893e3204f901c5a680e855ac4caa99602fc673a7
aboujaafar1997 <53882254+aboujaafar1997@users.noreply.github.com>
2019-10-23 19:36:37 +0100

    first

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -10,11 +10,11 @@ class Game extends Component {
         vsnbr: [],
         role: this.props.role,
     }
-    style={
-        "color":"white",
-        "fontSize":"20px"
+    style = {
+        "color": "white",
+        "fontSize": "20px"
     }
-  
+
     componentDidMount() {
         this.event();
         $("[type='number']").keypress(function (evt) {
@@ -49,7 +49,7 @@ class Game extends Component {
                         <div className="card text-white bg-primary mb-3" style={{ minHeight: '30rem' }} >
                             <div className="card-header">{this.props.utilisateur}</div>
                             <div className="card-body" style={this.style}>
-                            <center>{vs}</center>
+                                <center>{vs}</center>
                                 {/* {crr_vs} */}
                             </div>
                         </div>
@@ -59,9 +59,9 @@ class Game extends Component {
                     <div className="col-5">
                         <div className="card text-white bg-info mb-3" style={{ minHeight: '30rem' }} >
                             <div className="card-header">{this.props.vs_palyer}</div>
-                            <div className="card-body"style={this.style}>
+                            <div className="card-body" style={this.style}>
                                 <center>
-                                {me}</center>
+                                    {me}</center>
                                 {/* {crr_me} */}
                             </div>
                         </div>
@@ -69,19 +69,19 @@ class Game extends Component {
                     </div>
                 </div>
                 <div className="container">
-                <audio className="audio-element">
-          <source src="https://api.coderrocketfuel.com/assets/pomodoro-times-up.mp3"></source>
-            </audio>
+                    <audio className="audio-element">
+                        <source src="https://api.coderrocketfuel.com/assets/pomodoro-times-up.mp3"></source>
+                    </audio>
                     <div className="row">
                         <div className="col-2 col-sm-2 col-xm-2 "></div>
-                        <div className="col-2 col-sm-2 col-xm-2 "><input ref='r1' min="1" max= "9" class="form-control" type="number" id="example-number-input1" /></div>
-                        <div className="col-2 col-sm-2 col-xm-2 "><input ref='r2' min="1" max= "9" class="form-control" type="number" id="example-number-input2" /></div>
-                        <div className="col-2 col-sm-2 col-xm-2 "><input ref='r3' min="1" max= "9"class="form-control" type="number" id="example-number-input3" /></div>
-                        <div className="col-2 col-sm-2 col-xm-2 "><input min="1" max= "9" ref='r4' class="form-control" type="number" id="example-number-input4" /></div>
+                        <div className="col-2 col-sm-2 col-xm-2 "><input ref='r1' min="1" max="9" class="form-control" type="number" id="example-number-input1" /></div>
+                        <div className="col-2 col-sm-2 col-xm-2 "><input ref='r2' min="1" max="9" class="form-control" type="number" id="example-number-input2" /></div>
+                        <div className="col-2 col-sm-2 col-xm-2 "><input ref='r3' min="1" max="9" class="form-control" type="number" id="example-number-input3" /></div>
+                        <div className="col-2 col-sm-2 col-xm-2 "><input min="1" max="9" ref='r4' class="form-control" type="number" id="example-number-input4" /></div>
                     </div>
                     <br />
                     <br />
-                    {  this.props.vsnbr.length>0?this.state.role === 1 ? <button type="button" class="btn btn-light btn-lg btn-block" onClick={(e) =>{const audioEl = document.getElementsByClassName("audio-element")[0]; audioEl.play(); this.setnembre()}}>send</button> : <button type="button" disabled class="btn btn-light btn-lg btn-block" onClick={(e) => this.setnembre()}>send</button>:<div><div className="loader"></div>Loding...</div>}
+                    {this.props.vsnbr.length > 0 ? this.state.role === 1 ? <button type="button" class="btn btn-light btn-lg btn-block" onClick={(e) => { const audioEl = document.getElementsByClassName("audio-element")[0]; audioEl.play(); this.setnembre() }}>send</button> : <button type="button" disabled class="btn btn-light btn-lg btn-block" onClick={(e) => this.setnembre()}>send</button> : <div><div className="loader"></div>Loding...</div>}
                 </div>
             </div>
         );
@@ -122,7 +122,7 @@ class Game extends Component {
                     nbrdata_me: [...this.state.nbrdata_me, data.nbr],
                     role: this.state.role * -1
                 });
-                this.corecteur(data.nbr,this.props.vsnbr, data.utilisateur);
+                this.corecteur(data.nbr, this.props.vsnbr, data.utilisateur);
 
             }
             else {
@@ -130,22 +130,30 @@ class Game extends Component {
                     nbrdata_vs: [...this.state.nbrdata_vs, data.nbr],
                     role: this.state.role * -1
                 });
-                this.corecteur(data.nbr,this.props.mynbr, data.utilisateur);
+                this.corecteur(data.nbr, this.props.mynbr, data.utilisateur);
             }
         });
     }
 
-    corecteur = (tab,com, utilisateur) => {
-        var crr = [];  
-         for (let i = 0; i < com.length; i++) {
-            for (let j = 0; j < tab.length; j++) {
-                if (com[i] === tab[j]) {
+    corecteur = (tab, com, utilisateur) => {
+        var crr = [];
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                if (tab[i] === com[j]) {
                     if (i === j) {
                         crr[i] = "V"; break;
                     }
                     else { crr[i] = "R"; break; }
                 }
-                else crr[i] = "F";
+                else {
+
+                    if (j == 3) {
+                        crr[i] = 'F';
+                    }
+                    else {
+                        continue;
+                    }
+                }
             }
         }
         if (utilisateur === this.props.utilisateur) {
@@ -155,7 +163,7 @@ class Game extends Component {
         }
 
         if (crr[0] === "V" && crr[1] === "V" && crr[2] === "V" && crr[3] === "V") {
-            document.body.innerHTML="<center><h1 style='font-size:30rem; color:'green'>"+utilisateur+" Rbaahtii nice </h1></center>"
+            document.body.innerHTML = "<center><h1 style='font-size:30rem; color:'green'>" + utilisateur + " Rbaahtii nice </h1></center>"
         }
     }
 
